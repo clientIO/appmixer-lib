73a45686c09f07eebc76dea38cde65b9448f364f
Martin Krcmar <martin.krcmar1@gmail.com>
2018-01-02 18:13:28 +0100

    DB

--- appmixer-lib.js
+++ appmixer-lib.js
@@ -1,11 +1,12 @@
 'use strict';
 
 module.exports = {
+    apiDriver: require('./api-driver'),
+    db: require('./db/db'),
     util: {
         HttpError: require('./util/http-error'),
         object: require('./util/object'),
         component: require('./util/component'),
         PagingAggregator: require('./util/paging-aggregator')
-    },
-    apiDriver: require('./api-driver')
+    }
 };
