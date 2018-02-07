d14fc2f5cf9a54f712bd36abff1e7760a9741aa4
waldez <tomas.waldauf@gmail.com>
2018-02-07 15:56:09 +0100

    fix

--- util/array.js
+++ util/array.js
@@ -10,17 +10,14 @@ const check = require('check-types');
 module.exports.addUniqueToArray = function(source, target) {
 
     check.assert.array(source, 'Invalid source array.');
+    check.assert.array(target, 'Invalid target array.');
 
-    if (Array.isArray(target) && target.length > 1) {
-        const set = new Set(target.map(i => i.value));
-
-        for (let src of source) {
-            if (!set.has(src.value)) {
-                set.add(src.value);
-                target.push(src);
-            }
+    const set = new Set(target.map(i => i.value));
+    for (let src of source) {
+        if (!set.has(src.value)) {
+            set.add(src.value);
+            target.push(src);
         }
     }
-
-    return source || [];
+    return target || [];
 };
